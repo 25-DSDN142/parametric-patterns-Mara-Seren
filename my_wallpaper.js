bea53ebd4caeee7b8836175bf850111f1f6c1a30
Mara-Seren <maraseren14@gmail.com>
2025-08-01 14:36:15 +1200

    Final Wallpaper Code - Mara Barrett
    This is my final Code for Project One ready for hand in and marking

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -13,10 +13,10 @@ let ShowFlower2 = true; //false removes flower2/true adds flower2
 let Showbackground= true;//green background
 let Showbackground2= false;//blue background
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
